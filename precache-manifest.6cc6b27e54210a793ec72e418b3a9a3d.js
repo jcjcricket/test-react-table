84f0c3c81bffbbdef89a3bfc239aa0bf4b83c798
J CRICKET <sssssssnk7@gmail.com>
2020-10-25 12:14:09 +0200

    Updates

---
+++ precache-manifest.6cc6b27e54210a793ec72e418b3a9a3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c98147d317eb6275803b5a5dcb6d0f91",
+    "url": "/test-react-table/index.html"
+  },
+  {
+    "revision": "ac16ea3a108607129a68",
+    "url": "/test-react-table/static/css/main.43d226fc.chunk.css"
+  },
+  {
+    "revision": "7f2dff954211f7ba358b",
+    "url": "/test-react-table/static/js/2.038397cc.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/test-react-table/static/js/2.038397cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac16ea3a108607129a68",
+    "url": "/test-react-table/static/js/main.64fcbf9c.chunk.js"
+  },
+  {
+    "revision": "a1692551732a46ccd117",
+    "url": "/test-react-table/static/js/runtime-main.b571f730.js"
+  }
+]);
